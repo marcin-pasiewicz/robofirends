6ff4aa59c42410a4932f8e36177a8919f4d6c1ae
marcin-pasiewicz <marcin.pasiewicz@gmail.com>
2018-07-11 21:00:14 +0200

    init commit

---
+++ src/reducers.js
@@ -0,0 +1,55 @@
+import {
+    CHANGE_SEARCH_FILED,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAILED
+} from './constants';
+
+const initialStateSearch = {
+    searchField: '',
+};
+
+const initialStateRobots = {
+    isPending: false,
+    robots: [],
+    error: '',
+};
+
+export const searchRobots = (state = initialStateSearch, action = {}) => {
+    switch (action.type) {
+        case CHANGE_SEARCH_FILED:
+            return {
+                ...state,
+                searchField: action.payload,
+            };
+        default:
+            return state;
+    }
+};
+
+export const requestRobots = (state = initialStateRobots, action = {}) => {
+    switch (action.type) {
+        case REQUEST_ROBOTS_PENDING:
+            return {
+                ...state,
+                isPending: true,
+            };
+
+        case REQUEST_ROBOTS_SUCCESS:
+            return {
+                ...state,
+                isPending: false,
+                robots: action.payload,
+            };
+
+        case REQUEST_ROBOTS_FAILED:
+            return {
+                ...state,
+                isPending: false,
+                error: action.error,
+            };
+
+        default:
+            return state;
+    }
+};
